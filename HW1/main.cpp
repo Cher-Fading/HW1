4670a5ceb6c1ec86796b781af7367f89ff084ffc
Cher Wang <cherwang@chers-macbook-pro-2.local>
2017-09-04 13:14:45 -0400

    Initial Commit

---
+++ HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by Cher Wang on 9/4/17.
+//  Copyright © 2017 Cher Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
