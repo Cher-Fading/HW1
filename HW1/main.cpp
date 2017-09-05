07a3ea9339120aaa3e4f976c4b1a9cd7a0b47d70
Cher Wang <cherwang@chers-macbook-pro-2.local>
2017-09-04 23:11:43 -0400

    Finished 1abc

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -2,25 +2,27 @@
 //  main.cpp
 //  HW1
 //
-//  Created by Cher Wang on 9/4/17.
+//  Created by Xiaoning Wang on 9/4/17.
 //  Copyright © 2017 Cher Wang. All rights reserved.
+//  References used: https://stackoverflow.com/questions/572962/reading-a-matrix-txt-file-and-storing-as-an-array
 //
 
 #include <iostream>
 #include "nr3.h"
 #include "ludcmp.h"
+#include "svd.h"
 
 int main(int argc, const char * argv[]) {
     //Homework 1 Question 1
     // Initialize vectors and matrix A
     Doub a[2]={0.1869169, 0.2155734};
-    VecDoub b(2,a);
+    VecDoub b = VecDoub(2,a);
     Doub a1[2]={1.0,-1.0};
-    VecDoub x(2,a1);
+    VecDoub x = VecDoub(2,a1);
     Doub a2[2]={0.9999999,-1.0000001};
-    VecDoub x1(2,a2);
+    VecDoub x1 = VecDoub(2,a2);
     Doub a3[2]={0.4073666,-0.1945277};
-    VecDoub x2(2,a3);
+    VecDoub x2 = VecDoub(2,a3);
     MatDoub A(2,2);
     A[0][0]=0.7073725;
     A[0][1]=0.5204556;
@@ -29,12 +31,12 @@ int main(int argc, const char * argv[]) {
     
     //a)Compute residuals
     //Set up 2 vectors to contain residual vectors
-    VecDoub r1(2);
-    VecDoub r2(2);
+    VecDoub r1 = VecDoub(2);
+    VecDoub r2 = VecDoub(2);
     
     //Do matrix multiplication and vector subtraction
-    for (int i = 0; i<=1; i++){
-        for (int j = 0; j<=1; j++){
+    for (int i = 0; i<A.nrows(); i++){
+        for (int j = 0; j<A.ncols(); j++){
             r1[i]+=A[i][j]*x1[j];
             r2[i]+=A[i][j]*x2[j];
         }
@@ -59,9 +61,43 @@ int main(int argc, const char * argv[]) {
     
     //b)Compute residuals and error of using LU decomposition
     //Use NR routine of LU decomposition to construct a LUdcmp struct, and then use its method solve to solve for the given vector b.
+    std::cout<<std::endl<<"1. b)"<<std::endl;
     LUdcmp LUA = LUdcmp(A);
     VecDoub x_numerical(2);
     LUA.solve(b, x_numerical);
     
+    std::cout<<"x_numerical using LU decomposition routine is ("<<x_numerical[0]<<", "<<x_numerical[1]<<"). "<<std::endl;
+    VecDoub error_numerical  = VecDoub(2);
+    for (int i = 0; i<error_numerical.size(); i++){
+        error_numerical[i]=x_numerical[i]-x[i];
+    }
+    std::cout<<"x_numerical error using LU decomposition routine is ("<<error_numerical[0]<<", "<<error_numerical[1]<<"). "<<std::endl;
+    
+    VecDoub r_numerical = VecDoub(2);
+    for (int i = 0; i<A.nrows(); i++){
+        for (int j = 0; j<A.ncols(); j++){
+            r_numerical[i]+=A[i][j]*x_numerical[j];
+        }
+        r_numerical[i]=b[i]-r_numerical[i];
+    }
+    std::cout<<"x_numerical residual using LU decomposition routine is ("<<r_numerical[0]<<", "<<r_numerical[1]<<"). "<<std::endl;
+    Doub sr_numerical = sqrt(r_numerical[0]*r_numerical[0]+r_numerical[1]*r_numerical[1]);
+    Doub serror_numerical = sqrt(error_numerical[0]*error_numerical[0]+error_numerical[1]*error_numerical[1]);
+    Int order = log10(sr_numerical/serror_numerical);
+    std::cout<<"The solution residual is about an order of 10 to the exponent of "<<order<<" in comparison to the solution error."<<std::endl;
+    
+    //c) Compute condition number
+    std::cout<<std::endl<<"1. c)"<<std::endl;
+    SVD SVDA = SVD(A);
+    Doub condA_inv = SVDA.inv_condition();
+    std::cout<<"The condition number of A is "<<1./condA_inv<<std::endl;
+    std::cout<<"It's reciprocal is "<<condA_inv<<std::endl;
+    
+    //2.
+    //Firstly load files into matrix vector
+    //Reference https://stackoverflow.com/questions/572962/reading-a-matrix-txt-file-and-storing-as-an-array
+    //compute matrix inverse
+    
     return 0;
 }
+
